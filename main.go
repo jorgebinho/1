8705670c2052fb14fec56780d48b89d963f5de48
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-07-26 21:22:59 -0300

    :technologist: Arrays and for loops

--- main.go
+++ main.go
@@ -15,5 +15,13 @@ var (
 )
 
 func main() {
-	fmt.Printf("O tipo de E eh %T", e)
+	var myArray [3]int
+
+	myArray[0] = 10
+	myArray[1] = 20
+	myArray[2] = 30
+
+	for i, v := range myArray {
+		fmt.Printf("The value of index %d is %d\n", i, v)
+	}
 }
