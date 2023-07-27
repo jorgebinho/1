76e6e0194dfdb4b6072dc4f6855be99b89c9551f
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-07-26 21:10:17 -0300

    :technologist: Create of type

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 const a = "Hello, World!"
 
+type ID int
+
 var (
 	b bool    = true
 	c int     = 10
 	d string  = "Jorge"
 	e float64 = 1.2
+	g ID      = 3
 )
 
 func main() {
-	f := 10
 	println(a)
-	println(f)
+	println(g)
 }
