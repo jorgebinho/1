1807422c2bca899713a1bb7d25a711d85cee3439
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-07-26 21:07:47 -0300

    :technologist: Variables in go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Jorge"
+	e float64 = 1.2
+)
+
+func main() {
+	f := 10
+	println(a)
+	println(f)
+}
