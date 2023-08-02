dcf79c715499cadcdd74a136184b257a37d398d3
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-08-02 19:51:47 -0300

    :technologist: Closures

--- functions/main.go
+++ functions/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+
+	total := func() int {
+		return sum(50, 3, 70, 35, 78, 743) * 2
+
+	}()
+
+	fmt.Println(total)
+
 	fmt.Println(sum(50, 3, 70, 35, 78, 743))
 }
 
