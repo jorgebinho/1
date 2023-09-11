168d38e263bc8ef86a6bc699293aefa1d9355306
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-09-11 20:34:21 -0300

    :art: Baixar pacotes

--- pacotes/main.go
+++ pacotes/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"matematica/math"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 	s := math.Soma(10, 20)
 	fmt.Println("Resultado: ", s)
+	fmt.Println(uuid.New())
 }
