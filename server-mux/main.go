7403b43e03eb619e6ff1bbef238c0b3c5237f76f
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-09-13 19:47:31 -0300

    :technologist: Server MUX

---
+++ server-mux/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "My blog"})
+	http.ListenAndServe(":8080", mux)
+
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Jorge"))
+	})
+	http.ListenAndServe(":8081", mux2)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
+}
